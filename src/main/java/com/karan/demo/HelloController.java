2abd7f4bca27624a5bf821a75ae7c0f2163fa981
karanevive <karan.arora@evivehealth.com>
2017-12-21 01:42:17 +0530

    initial commit

---
+++ src/main/java/com/karan/demo/HelloController.java
@@ -0,0 +1,21 @@
+package com.karan.demo;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Rest Contoller Combines - Controller and Response body - Read more about it!!!   -   15/12/2017
+ * two annotations that results in web requests returning data, rather than a view.
+ */
+@Controller
+@RestController
+public class HelloController {
+
+  @RequestMapping("/")
+  public String index() {
+    return "Greetings!! Karan Arora";
+  }
+
+}
